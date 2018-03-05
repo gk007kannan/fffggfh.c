34ba62bdac47c3ac8bed09ffb86de2e95b1fc780
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-03-05 15:45:56 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+#include<conio.h>
+int main() 
+{
+char a[100],i;
+clrscr();
+printf("\n enter the worde of a");
+scanf("\n %d",&a);
+for(i=0;i<a;i++)
+{
+printf("\n hai gk electronics");
+}
+getch();
+}
